9162a2cac74d33dd01a2e3ffd83e959529d07300
Volodymyr Denchyk <vdenchyk@vdenchyk-mbp.local>
2024-12-06 16:48:39 +0200

    INTAP-1743: step 4

---
+++ src/Training/Bundle/UserNamingBundle/Twig/UserNamingExtension.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Training\Bundle\UserNamingBundle\Twig;
+
+use Oro\Bundle\UserBundle\Entity\User;
+
+use Training\Bundle\UserNamingBundle\Provider\UserFormattedNameProvider;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class UserNamingExtension extends AbstractExtension
+{
+    /**
+     * @param UserFormattedNameProvider $formattedNameProvider
+     */
+    public function __construct(private UserFormattedNameProvider $formattedNameProvider)
+    {
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('formatted_name_example', [$this, 'getFormattedNameExample']),
+            new TwigFunction('formatted_name', [$this, 'getRealFormattedName']),
+        ];
+    }
+
+    /**
+     * @param string $format
+     * @return string
+     */
+    public function getFormattedNameExample(string $format): string
+    {
+        $user = new User();
+        $user->setNamePrefix('Mr.')
+            ->setFirstName('John')
+            ->setMiddleName('M')
+            ->setLastName('Doe')
+            ->setNameSuffix('Jr.');
+
+        return $this->formattedNameProvider->getFormattedName($user, $format);
+    }
+
+    /**
+     * @param string|null $format
+     * @param string|null $firstName
+     * @param string|null $lastName
+     * @param string|null $middleName
+     * @param string|null $namePrefix
+     * @param string|null $nameSuffix
+     * @return string
+     */
+    public function getRealFormattedName(
+        string $format = null,
+        string $firstName = null,
+        string $middleName = null,
+        string $lastName = null,
+        string $namePrefix = null,
+        string $nameSuffix = null
+    ): string
+    {
+        $user = new User();
+        $user->setNamePrefix($namePrefix)
+            ->setFirstName($firstName)
+            ->setMiddleName($middleName)
+            ->setLastName($lastName)
+            ->setNameSuffix($nameSuffix);
+
+        return $this->formattedNameProvider->getFormattedName($user, $format);
+    }
+}
